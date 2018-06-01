0786bba869699e98ba2f1b7c37cb60845ed5edb1
René Schollmeyer <rschollmeyer@evoila.de>
2018-06-01 16:46:56 +0200

    chore(submodule): version bump

--- osb-service/src/main/java/de/evoila/cf/broker/custom/LogMetricPlatformService.java
+++ osb-service/src/main/java/de/evoila/cf/broker/custom/LogMetricPlatformService.java
@@ -2,17 +2,13 @@ package de.evoila.cf.broker.custom;
 
 import de.evoila.cf.broker.exception.PlatformException;
 import de.evoila.cf.broker.model.Plan;
-import de.evoila.cf.broker.model.Platform;
 import de.evoila.cf.broker.model.ServiceInstance;
-import de.evoila.cf.broker.repository.PlatformRepository;
-import de.evoila.cf.broker.service.PlatformService;
+import de.evoila.cf.cpi.existing.ExistingServiceFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.util.Map;
 
 /**
@@ -20,34 +16,9 @@ import java.util.Map;
  */
 @Service
 @EnableConfigurationProperties
-public class LogMetricPlatformService implements PlatformService {
+public class LogMetricPlatformService extends ExistingServiceFactory {
 
     private final Logger log = LoggerFactory.getLogger(LogMetricPlatformService.class);
-
-    @Autowired
-    private PlatformRepository platformRepository;
-
-    @Override
-    @PostConstruct
-    public void registerCustomPlatformService() {
-        if(platformRepository != null) {
-            platformRepository.addPlatform(Platform.EXISTING_SERVICE, this);
-        }
-    }
-
-    @Override
-    public boolean isSyncPossibleOnCreate(Plan plan) { return false; }
-
-    @Override
-    public boolean isSyncPossibleOnDelete(ServiceInstance serviceInstance) { return false; }
-
-    @Override
-    public boolean isSyncPossibleOnUpdate(ServiceInstance serviceInstance, Plan plan) { return false; }
-
-    @Override
-    public ServiceInstance preCreateInstance(ServiceInstance serviceInstance, Plan plan) throws PlatformException {
-        return serviceInstance;
-    }
 
     @Override
     public ServiceInstance createInstance(ServiceInstance serviceInstance, Plan plan, Map<String, Object> customParameters) throws PlatformException {
@@ -57,20 +28,8 @@ public class LogMetricPlatformService implements PlatformService {
     }
 
     @Override
-    public ServiceInstance getCreateInstancePromise(ServiceInstance serviceInstance, Plan plan) { return serviceInstance; }
-
-    @Override
     public ServiceInstance postCreateInstance(ServiceInstance serviceInstance, Plan plan) throws PlatformException { return serviceInstance; }
 
     @Override
-    public void preDeleteInstance(ServiceInstance serviceInstance) throws PlatformException { }
-
-    @Override
     public void deleteInstance(ServiceInstance serviceInstance, Plan plan) throws PlatformException { }
-
-    @Override
-    public void postDeleteInstance(ServiceInstance serviceInstance) throws PlatformException { }
-
-    @Override
-    public ServiceInstance updateInstance(ServiceInstance serviceInstance, Plan plan, Map<String, Object> customParameters) throws PlatformException { return serviceInstance; }
 }
